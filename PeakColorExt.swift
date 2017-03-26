172a16982ea0ec98a2c5a02c82be08fd9ec9e40c
Connor Monks <connormonks@dhcp-wifi-8021x-155-41-12-114.bu.edu>
2017-03-25 22:09:21 -0400

    Updated the comments again

--- PeakColorExt.swift
+++ PeakColorExt.swift
@@ -14,6 +14,6 @@ extension UIColor{
     //The Peak App Main Color
     static var peakColor: UIColor {return UIColor(red: 30/255, green: 220/255, blue: 187/255, alpha: 1.0)}
     
-    //The Color of the artist Title
+    //The Color of the Artist Title
     static var artistColor: UIColor {return UIColor(red: 255/255, green: 60/255, blue: 60/255, alpha: 1.0)}
 }
