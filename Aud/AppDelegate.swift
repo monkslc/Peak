5027512fe6176e8421b18d4ff9be21b63f7ebd7a
Cameron Monks <ccmonks32@gmail.com>
2017-03-31 14:26:47 -0400

    Songs now pause when you close the app

--- Aud/AppDelegate.swift
+++ Aud/AppDelegate.swift
@@ -57,6 +57,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        peakMusicController.systemMusicPlayer.pause()
     }
 
     func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
