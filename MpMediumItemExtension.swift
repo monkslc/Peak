4620149ad6a708bf7cc1e844f5f2ca0bbf0669e6
Connor Monks <connormonks@macbook-pro.local>
2017-05-30 22:05:16 -0400

    Fixed Apple Music Artist Search

--- MpMediumItemExtension.swift
+++ MpMediumItemExtension.swift
@@ -18,7 +18,7 @@ extension MPMediaItem: BasicSong {
     func getId() -> String { return "\(self.playbackStoreID)" }
     func getTrackName() -> String { return self.title! }
     func getCollectionName() -> String { return albumTitle! }
-    func getArtistName() -> String { return albumArtist! }
+    func getArtistName() -> String { return artist ?? "No Artist" }
     func getTrackTimeMillis() -> Int { return Int(self.playbackDuration) }
     func getImage() -> UIImage? { return artwork?.image(at: CGSize()) }
     func getDateAdded() -> Date? { return dateAdded }
