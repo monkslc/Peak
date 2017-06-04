932ae9273125433c5da7315e83c3c0e112eb01c1
Connor Monks <connormonks@macbook-pro.local>
2017-06-04 16:05:32 -0400

    Created New Branch

---
+++ GroupsViewwController.swift
@@ -0,0 +1,32 @@
+//
+//  GroupsViewwController.swift
+//  Peak
+//
+//  Created by Connor Monks on 6/4/17.
+//  Copyright © 2017 Connor Monks. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+
+class GroupsViewwController: UIViewController {
+
+    /*MARK: PROPERTIES*/
+    
+    @IBOutlet weak var facebookLoginButton: FBSDKLoginButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //Set our permissions for Facebook 
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
